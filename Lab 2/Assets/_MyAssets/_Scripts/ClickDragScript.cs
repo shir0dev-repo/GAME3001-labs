8feae8c8e8ca70e07fb7ad3fda3fd1905a1aeecb
shir0dev-repo <v010000010101001101010011@gmail.com>
2024-01-16 16:14:09 -0500

    added drag offset to gameobject

--- Lab 2/Assets/_MyAssets/_Scripts/ClickDragScript.cs
+++ Lab 2/Assets/_MyAssets/_Scripts/ClickDragScript.cs
@@ -6,8 +6,11 @@ public class ClickDragScript : MonoBehaviour
 {
     bool m_isDragging = false;
     private Rigidbody2D m_currentDraggedBody;
+    private Vector3 m_dragOffset = Vector3.zero;
+    private Vector2 m_mousePosition = Vector2.zero;
 
     private Camera m_mainCam;
+
 
     private void Awake()
     {
@@ -16,11 +19,14 @@ public class ClickDragScript : MonoBehaviour
 
     private void Update()
     {
+        // update current mouse position.
+        m_mousePosition = m_mainCam.ScreenToWorldPoint(Input.mousePosition);
+
         // listen for mouse click
         if(Input.GetMouseButtonDown(0))
         {
             // raycast from mouse position
-            RaycastHit2D hit = Physics2D.Raycast(m_mainCam.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            RaycastHit2D hit = Physics2D.Raycast(m_mousePosition, Vector2.zero);
 
             // gameobject has both a collider and a rigidbody2D
             if (hit.collider != null && hit.rigidbody != null && hit.rigidbody != m_currentDraggedBody)
@@ -28,6 +34,7 @@ public class ClickDragScript : MonoBehaviour
                 // start dragging if no other object is being dragged.
                 m_isDragging = true;
                 m_currentDraggedBody = hit.rigidbody;
+                m_dragOffset = m_currentDraggedBody.position - m_mousePosition;
             }
         }
 
@@ -35,11 +42,12 @@ public class ClickDragScript : MonoBehaviour
         {
             m_isDragging = false;
             m_currentDraggedBody = null;
+            m_dragOffset = Vector2.zero;
         }
 
         if (m_isDragging && m_currentDraggedBody != null)
         {
-            m_currentDraggedBody.MovePosition(m_mainCam.ScreenToWorldPoint(Input.mousePosition));
+            m_currentDraggedBody.MovePosition(m_dragOffset + m_mainCam.ScreenToWorldPoint(Input.mousePosition));
         }
     }
 
